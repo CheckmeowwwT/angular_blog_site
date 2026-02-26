4c412af743f7006b3c7823a72fcea309921b7449
checkmeowwwT <162413040+CheckmeowwwT@users.noreply.github.com>
2026-02-25 22:36:07 -0500

    add environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://fvfybrvlxtkopzocqmgm.supabase.co',
+    supabaseAnonKey: 'sb_publishable_vUaR6PEhd_fTFKmEtOlMsg_K2hifTMH',
+  };
